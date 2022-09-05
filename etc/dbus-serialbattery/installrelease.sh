051ea629ab1a7cc887d13ec5a8a53068af366bb6
Nathan Gardiner <ngardiner@gmail.com>
2022-09-05 22:51:02 +1000

    Update installrelease.sh
    bugfix - wrong file format

--- etc/dbus-serialbattery/installrelease.sh
+++ etc/dbus-serialbattery/installrelease.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://github.com/ngardiner/dbus-serialbattery-withpzem/archive/refs/heads/withpzem.zip -O venus-data.tar.gz -qi - 
+wget https://github.com/ngardiner/dbus-serialbattery-withpzem/archive/refs/heads/withpzem.tar.gz -O venus-data.tar.gz -qi - 
 tar -zxf ./venus-data.tar.gz -C /data
 sh /data/etc/dbus-serialbattery/reinstalllocal.sh
