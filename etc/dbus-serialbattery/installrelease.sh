a0206a7965514a8c0b681c9732343ab898973488
Nathan Gardiner <ngardiner@gmail.com>
2022-09-06 16:59:27 +1000

    Update installrelease.sh
    Increase verbosity

--- etc/dbus-serialbattery/installrelease.sh
+++ etc/dbus-serialbattery/installrelease.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://github.com/ngardiner/dbus-serialbattery-withpzem/archive/refs/heads/withpzem.tar.gz -O /tmp/venus-data.tar.gz -qi - 
-tar -zxf /tmp/venus-data.tar.gz --strip-components=1 -C /data
-sh /data/etc/dbus-serialbattery/reinstalllocal.sh
+wget https://github.com/ngardiner/dbus-serialbattery-withpzem/archive/refs/heads/withpzem.tar.gz -O /data/venus-data.tar.gz -qi - 
+tar -vzxf /data/venus-data.tar.gz --strip-components=1 -C /data
+bash -x /data/etc/dbus-serialbattery/reinstalllocal.sh
