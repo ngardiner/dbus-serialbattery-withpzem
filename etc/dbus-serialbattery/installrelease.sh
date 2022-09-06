22187f7f5857e3de144dc554eb858bc244b790e4
Nathan Gardiner <ngardiner@gmail.com>
2022-09-06 17:01:31 +1000

    Update installrelease.sh
    Fix script

--- etc/dbus-serialbattery/installrelease.sh
+++ etc/dbus-serialbattery/installrelease.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://github.com/ngardiner/dbus-serialbattery-withpzem/archive/refs/heads/withpzem.tar.gz -O /data/venus-data.tar.gz -qi - 
+wget -q https://github.com/ngardiner/dbus-serialbattery-withpzem/archive/refs/heads/withpzem.tar.gz -O /data/venus-data.tar.gz
 tar -vzxf /data/venus-data.tar.gz --strip-components=1 -C /data
 bash -x /data/etc/dbus-serialbattery/reinstalllocal.sh
